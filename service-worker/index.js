33e64d9b96c12d015d7a030f6b58885d95d734d7
Maciej Kwaśniak <kmaciek17@gmail.com>
2019-06-01 10:43:59 +0200

    Fix fetching

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,27 +1,35 @@
-import {
-  INDEX_HTML_PATH,
-  ENVIRONMENT,
-  VERSION,
-  INDEX_EXCLUDE_SCOPE,
-  INDEX_INCLUDE_SCOPE
-} from 'ember-service-worker-index/service-worker/config';
-
-import { urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
+import { ENVIRONMENT, INDEX_EXCLUDE_SCOPE, INDEX_HTML_PATH, INDEX_INCLUDE_SCOPE, VERSION } from 'ember-service-worker-index/service-worker/config';
 import cleanupCaches from 'ember-service-worker/service-worker/cleanup-caches';
+import { urlMatchesAnyPattern } from 'ember-service-worker/service-worker/url-utils';
+
 
 const CACHE_KEY_PREFIX = 'esw-index';
 const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
 
 const INDEX_HTML_URL = new URL(INDEX_HTML_PATH, self.location).toString();
 
+const _fetchIndex = function() {
+  return fetch(INDEX_HTML_URL, { credentials: 'include' }).then((response) => {
+    caches.open(CACHE_NAME).then((cache) => cache.put(INDEX_HTML_URL, response));
+    return response;
+  })
+};
+
+const _returnCachedIndex = function() {
+  return caches.match(INDEX_HTML_URL, { cacheName: CACHE_NAME }).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      // Re-fetch the resource in the event that the cache had been cleared
+      // (mostly an issue with Safari 11.1 where clearing the cache causes
+      // the browser to throw a non-descriptive blank error page).
+      return _fetchIndex();
+  });
+};
+
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    fetch(INDEX_HTML_URL, { credentials: 'include' }).then((response) => {
-      return caches
-        .open(CACHE_NAME)
-        .then((cache) => cache.put(INDEX_HTML_URL, response));
-    })
-  );
+  event.waitUntil(_fetchIndex());
 });
 
 self.addEventListener('activate', (event) => {
@@ -39,24 +47,6 @@ self.addEventListener('fetch', (event) => {
   let isTests = url.pathname === '/tests' && ENVIRONMENT === 'development';
 
   if (!isTests && isGETRequest && isHTMLRequest && isLocal && scopeIncluded && !scopeExcluded) {
-    event.respondWith(
-      fetch(INDEX_HTML_URL, { credentials: 'include' }).catch(() => {
-        caches.match(INDEX_HTML_URL, { cacheName: CACHE_NAME })
-          .then((response) => {
-            if (response) {
-              return response;
-            }
-
-            // Re-fetch the resource in the event that the cache had been cleared
-            // (mostly an issue with Safari 11.1 where clearing the cache causes
-            // the browser to throw a non-descriptive blank error page).
-            return fetch(INDEX_HTML_URL, { credentials: 'include' })
-              .then((fetchedResponse) => {
-                caches.open(CACHE_NAME).then((cache) => cache.put(INDEX_HTML_URL, fetchedResponse));
-                return fetchedResponse.clone();
-            });
-        });
-      })
-    )
+    event.respondWith(_fetchIndex().catch(() => _returnCachedIndex()))
   }
 });
